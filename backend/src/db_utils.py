a50b2855930633db7ed5abfdcaa13e27a2f4abb0
Karttot <carrotskychenchilla@gmail.com>
2025-12-28 23:52:24 -0500

    merge conflict resolve

--- backend/src/db_utils.py
+++ backend/src/db_utils.py
@@ -3,9 +3,12 @@
 class Db_utils:
     def insert_row(self) -> None:
         #TODO
-        print("worked")
+        print("worked insert_row")
 
     def find_primary_keys(self) -> list[int]:
         #TODO 
-        print("worked 2") 
+        print("worked find_primary_keys") 
         #blah
+
+    def delete_row(self):
+        print("worked delete_row")
